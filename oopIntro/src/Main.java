89bc326797c6bbc96c05a387daa745f4eff255ba
ouzzkp <oguzkaan3096@gmail.com>
2023-03-19 13:01:00 +0300

    oopIntro_v1_2

--- oopIntro/src/Main.java
+++ oopIntro/src/Main.java
@@ -20,10 +20,28 @@ public class Main {
         product1.unitPrice = 15000;
         product1.detail = "16 GB Ram";
 
+        Product product2 = new Product(2, "Lenovo V15", 15000, "16 GB Ram"); // create a instance, referance
+
+        Product product3 = new Product(2, "HP 5", 15000, "24 GB Ram"); // create a instance, referance
+
         Product[] products = {
                 product1,
+                product2,
+                product3
         };
+
+        for (Product product : products) {
+            System.out.println(product.name + " ");
+        }
         System.out.println(products.length);
+
+        Category category1 = new Category();
+        category1.id = 1;
+        category1.name = "Bilgisayar";
+
+        Category category2 = new Category();
+        category2.id = 2;
+        category2.name = "Ev/Bahçe";
 
     }
 }
--- oopIntro/src/Product.java
+++ oopIntro/src/Product.java
@@ -1,4 +1,20 @@
+
 public class Product {
+
+    // Constructor Block
+    public Product() {
+        System.out.println("New instance created!");
+    }
+
+    // if signatures (name of the constructor and inside the "()" ) are same, the
+    // program will angry. To solve this issue, give a parameter.
+    public Product(int id, String name, double unitPrice, String detail) {
+        this.id = id; // id that inside the class
+        this.name = name; // name that inside the class
+        this.unitPrice = unitPrice; // unitPrice that inside the class
+        this.detail = detail; // detail that inside the class
+        System.out.println(name + " created with contrustor that has accept parameter!");
+    }
 
     int id;
     String name;
