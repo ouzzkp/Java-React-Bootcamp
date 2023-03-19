5a060bacc690155da4a2240d1593bc8f2074ae01
ouzzkp <oguzkaan3096@gmail.com>
2023-03-19 17:51:23 +0300

    Inheritance_intro

---
+++ Inheritance2/src/EmailLogger.java
@@ -0,0 +1,7 @@
+public class EmailLogger extends Logger {
+    // method overriding
+    @Override
+    public void log() {
+        System.out.println("Email yollandı");
+    }
+}
---
+++ Inheritance2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        CustomerService customerService = new CustomerService();
+        customerService.addGoodExample(new DatabaseLogger());
+        customerService.addGoodExample(new EmailLogger());
+
+    }
+}
